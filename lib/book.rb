2b8d1bd631087c54141d1ea94504091107eb2617
Sergio <kurisergio777777@gmail.com>
2022-05-20 19:30:29 -0400

    commit tree 3556f874b50aa4c301e73149737efa85f7e26995

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+require "pry"
 
+class Book 
+    attr_accessor :title 
+    attr_accessor :author 
+    attr_accessor :page_count 
+    attr_accessor :genre
+    attr_accessor :turn_page
+
+   def initialize(title)
+    @title = title 
+end 
+end 
+
+happybook = Book.new("And Then There Were None")
+happybook.turn_page="Flipping the page...wow, you read fast!"
 
